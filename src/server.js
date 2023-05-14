666636fd5d920ceb515369fff06304c8cfe80332
Sergei Biryukov <svbiryukov@luxoft.com>
2023-05-15 00:43:24 +0200

    set app version to 1

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const port = 8080
 const MY_VAR = process.env.MY_VAR || 'default'
 
 app.get('/', (req, res) => {
-  res.send('This is a test NodeJS V2 application. MY_VAR is ' + MY_VAR + '.')
+  res.send('This is a test NodeJS V1 application. MY_VAR is ' + MY_VAR + '.')
 })
 
 app.listen(port, '0.0.0.0', () => {
